b3e31cc4f41d33ff4aa8fe3c7e49a73ee708a0d2
mista-look <gibbard27@gmail.com>
2021-05-31 20:26:39 +0100

    added animations

---
+++ js/mainFunctions.js
@@ -0,0 +1,28 @@
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in")
+
+let observerOptions = {
+    root: null,
+    rootMargin: "0px 0px -100px 0px",
+    threshold: 0
+  };
+
+appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+          return;
+        } else{
+          entry.target.classList.add('appear');
+          appearOnScroll.unobserve(entry.target);
+          //console.log("in view");
+        }
+      });
+}, observerOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+})
