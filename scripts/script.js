cc22222e7e96c59b80c7aeaa2ac439a616686438
Luke Gibbard <gibbard27@gmail.com>
2023-07-11 21:31:05 +0100

    WIP: make responsive

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector(".burger-menu");
+const navMenu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
